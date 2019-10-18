c58dac48b6623b44d6e120ea39904c3bbeb84241
Cedric Travelletti <cedrictravelletti@gmail.com>
2019-10-18 10:38:39 +0200

    Added debug prints to identify branching in RBFkernel forward.

--- gpytorch/kernels/rbf_kernel.py
+++ gpytorch/kernels/rbf_kernel.py
@@ -78,9 +78,11 @@ class RBFKernel(Kernel):
         ):
             x1_ = x1.div(self.lengthscale)
             x2_ = x2.div(self.lengthscale)
+            print("In if.")
             return self.covar_dist(x1_, x2_, square_dist=True, diag=diag,
                                    dist_postprocess_func=postprocess_rbf,
                                    postprocess=True, **params)
+        print("Not in if.")
         return RBFCovariance().apply(x1, x2, self.lengthscale,
                                      lambda x1, x2: self.covar_dist(x1, x2,
                                                                     square_dist=True,
